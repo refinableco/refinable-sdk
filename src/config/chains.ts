140a1f9aacd5e680f25159ff7d240019cdc75a56
Jonas Snellinckx <jo.snel@hotmail.com>
2021-11-29 18:35:05 +0100

    fix(ENG-1362): use correct 6 decimals for polygon usdt (#22)

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -159,7 +159,7 @@ export const chainMap: Record<Chain, IChainConfig> = {
       {
         name: "USDT",
         symbol: "USDT",
-        decimals: 18,
+        decimals: 6,
         address: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
       },
       {
