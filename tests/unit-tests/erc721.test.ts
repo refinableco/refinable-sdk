cca79361aea96988a2368c29af00626b08d66aa8
Maxim Geerinck <maximgeerinck@hotmail.com>
2022-02-16 13:58:47 +0100

    feat: Add thumbnail possibility

--- tests/unit-tests/erc721.test.ts
+++ tests/unit-tests/erc721.test.ts
@@ -52,6 +52,29 @@ describe("Refinable", () => {
     expect(minredItem.supply).toEqual(1);
     expect(minredItem.contractAddress).toBeDefined();
   });
+  it("Should mint an ERC721 token with video and image", async () => {
+    const fileStream = fs.createReadStream(
+      path.resolve(__dirname, "../assets/image.jpg")
+    );
+    const videoFileStream = fs.createReadStream(
+      path.resolve(__dirname, "../assets/video.mp4")
+    );
+    const nft = await refinable
+      .nftBuilder()
+      .erc721({
+        nftFile: videoFileStream,
+        thumbnailFileStream: fileStream,
+        description: "some test description",
+        name: "The Test NFT",
+        royalty: new StandardRoyaltyStrategy([]),
+        chainId: Chain.Local,
+      })
+      .createAndMint();
+    const minredItem = nft.getItem();
+    expect(minredItem.chainId).toEqual(Chain.Local);
+    expect(minredItem.supply).toEqual(1);
+    expect(minredItem.contractAddress).toBeDefined();
+  });
   it("should put for sale", async () => {
     const fileStream = fs.createReadStream(
       path.resolve(__dirname, "../assets/image.jpg")
