4ed9ec306026bd4791b62504d7ac2a884e4c076f
Mihai Bercu <mihaiberq@gmail.com>
2022-03-23 09:10:49 +0200

    fix: Use RefinableTokenWhitelistedV3 contracts abi/bytecode, add env settings

--- src/config/sdk.ts
+++ src/config/sdk.ts
@@ -9,6 +9,25 @@ export const apiUrl = {
   [Environment.Local]: "http://localhost:8001/graphql",
 };
 
+export const contractMetadata = {
+  [Environment.Mainnet]: "https://api.refinable.com/contractMetadata/{address}",
+  [Environment.Testnet]:
+    "https://api-testnet.refinable.com/contractMetadata/{address}",
+  [Environment.Local]: "http://localhost:8001/contractMetadata/{address}",
+};
+
+export const ipfsUrl = {
+  [Environment.Mainnet]: "https://ipfs.refinable.com/ipfs/",
+  [Environment.Testnet]: "https://ipfs-testnet.refinable.com/ipfs/",
+  [Environment.Local]: "http://localhost:8080/ipfs/",
+};
+
+export const signer = {
+  [Environment.Mainnet]: "0xD2E49cfd5c03a72a838a2fC6bB5f6b46927e731A",
+  [Environment.Testnet]: "0xd4039eB67CBB36429Ad9DD30187B94f6A5122215",
+  [Environment.Local]: "0xd4039eB67CBB36429Ad9DD30187B94f6A5122215",
+};
+
 export const getContractsTags = (
   environment: Environment,
   chainId: Chain
