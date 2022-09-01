29c09eabf8a76f832e63c43f4eaa594c246fdeef
Jonas Snellinckx <jonas.snellinckx@gmail.com>
2022-09-01 14:33:41 +0200

    feat(DEV-2432): support APE payment

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -232,6 +232,12 @@ export const chainMap: Record<Chain, IChainConfig> = {
         decimals: 18,
         address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
       },
+      {
+        name: 'APE',
+        symbol: 'APE',
+        decimals: 18,
+        address: '0x4d224452801ACEd8B2F0aebE155379bb5D594381',
+      },
     ],
   },
   /**
