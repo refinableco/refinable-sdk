b97cbcf7bca01dc2fbdf8614dd6845a63c474071
Jonas Snellinckx <jonas.snellinckx@gmail.com>
2022-09-13 14:57:24 +0200

    chore: bump reservoir sdk (#128)
    * chore: bump reservoir sdk
    
    * chore: bump version

--- src/platform/LooksrarePlatform.ts
+++ src/platform/LooksrarePlatform.ts
@@ -1,15 +1,15 @@
 import { splitSignature } from "ethers/lib/utils";
 import { PartialOffer } from "../offer/Offer";
 import { AbstractPlatform } from "./AbstractPlatform";
-import * as reservoirSdk from "@refinableco/reservoir-sdk";
+import { LooksRare } from "@refinableco/reservoir-sdk";
 import { BytesEmpty } from "@refinableco/reservoir-sdk/dist/utils";
 
 export class LooksrarePlatform extends AbstractPlatform {
   buy(offer: PartialOffer, contractAddress: string, tokenId: string) {
     const { v, r, s } = splitSignature(offer.orderParams.signature);
 
-    const exchange = new reservoirSdk.LooksRare.Exchange(1);
-    const order = new reservoirSdk.LooksRare.Order(1, {
+    const exchange = new LooksRare.Exchange(1);
+    const order = new LooksRare.Order(1, {
       ...offer.orderParams,
       collection: contractAddress,
       tokenId: tokenId,
