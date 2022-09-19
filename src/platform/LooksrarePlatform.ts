9f65188470eb072eabeeb0bdca54506efe88240f
Maxim Geerinck <maxim@refinable.com>
2022-09-19 16:49:11 +0200

    feat: List for sale on looksrare

--- src/platform/LooksrarePlatform.ts
+++ src/platform/LooksrarePlatform.ts
@@ -3,6 +3,22 @@ import { PartialOffer } from "../offer/Offer";
 import { AbstractPlatform } from "./AbstractPlatform";
 import { LooksRare } from "@refinableco/reservoir-sdk";
 import { BytesEmpty } from "@refinableco/reservoir-sdk/dist/utils";
+import { Types } from "@refinableco/reservoir-sdk/dist/looks-rare";
+import {
+  ListApproveStatus,
+  ListSignStatus,
+  ListStatus,
+  LIST_STATUS_STEP,
+} from "../nft/interfaces/SaleStatusStep";
+import { MutationLooksrareListForSaleArgs, Platform } from "../@types/graphql";
+import axios from "axios";
+import { gql } from "graphql-request";
+
+export const LOOKSRARE_LIST_FOR_SALE = gql`
+  mutation looksrareListForSale($input: LooksrareListForSaleInput!) {
+    looksrareListForSale(input: $input)
+  }
+`;
 
 export class LooksrarePlatform extends AbstractPlatform {
   buy(offer: PartialOffer, contractAddress: string, tokenId: string) {
@@ -35,18 +51,76 @@ export class LooksrarePlatform extends AbstractPlatform {
     return unsignedTx;
   }
 
-  listForSale(offer: PartialOffer, contractAddress: string, tokenId: string) {
-    throw new Error("Not implemented yet.");
-    // const order = new LooksRare.Order(1, {
-    //   ...offer.orderParams,
-    //   collection: contractAddress,
-    //   tokenId: tokenId,
-    //   kind: "single-token",
-    //   params: BytesEmpty,
-    // });
+  async listForSale(
+    orderParams: Types.MakerOrderParams,
+    options: {
+      onProgress?: <T extends ListStatus>(status: T) => void;
+      onError?: (
+        {
+          step,
+          platform,
+        }: { step: LIST_STATUS_STEP; platform: Platform.Looksrare },
+        error
+      ) => void;
+    }
+  ) {
+    // approve
+    options.onProgress<ListApproveStatus>({
+      platform: Platform.Looksrare,
+      step: LIST_STATUS_STEP.APPROVE,
+      data: {
+        addressToApprove: "0x00000000000000000",
+      },
+    });
 
-    // console.log(order);
+    // sign
+    const nonce = await this.getNonce(orderParams.signer);
 
-    // order.params
+    const order = new LooksRare.Order(1, {
+      // looksrare params
+      ...orderParams,
+      nonce,
+
+      // reservoir specific params
+      kind: "single-token",
+    });
+    options.onProgress<ListSignStatus>({
+      platform: Platform.Looksrare,
+      step: LIST_STATUS_STEP.SIGN,
+      data: {
+        hash: order.getSignatureData().value,
+        what: "Looksrare order",
+      },
+    });
+    const signature = await this.refinable.account.sign(
+      order.getSignatureData()
+    );
+
+    // create
+    const response = await this.refinable.graphqlClient.request<
+      string,
+      MutationLooksrareListForSaleArgs
+    >(LOOKSRARE_LIST_FOR_SALE, {
+      input: { ...order.params, signature },
+    });
+
+    console.log({ response });
+
+    return order;
+  }
+
+  private async getNonce(makerAddress: string) {
+    // TODO: We should only use LooksRare's nonce when cross-posting to their orderbook
+    const nonce = await axios
+      .get(
+        `https://api.looksrare.org/api/v1/orders/nonce?address=${makerAddress}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then(({ data }: { data: { data: string } }) => data.data);
+    return nonce;
   }
 }
