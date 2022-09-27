0ca1f4b4a3cdcc93b703e3ae1d902c2b3204c798
Jonas Snellinckx <jonas.snellinckx@gmail.com>
2022-09-27 18:21:41 +0200

    improvement: less error prone simulation

--- src/transaction/simulate.ts
+++ src/transaction/simulate.ts
@@ -11,21 +11,11 @@ export const simulateUnsignedTx = async ({
   to: string;
   value: string;
 }) => {
-  // simulate
-  const gasLimit = await refinable.evm.signer.estimateGas({
-    data,
-    to,
-    value,
-  });
-
-  const feeData = await refinable.evm.signer.getFeeData();
   const body = {
     network_id: "1",
     from: refinable.accountAddress,
     to: to,
     input: data,
-    gas: gasLimit.toNumber() * 2,
-    gas_price: feeData.gasPrice.toNumber(),
     value: Number(value),
     save_if_fails: true,
   };
