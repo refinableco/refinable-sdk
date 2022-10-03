5f09fc2332d9d8c4f4c2ae56794cb2560094fa51
Jonas Snellinckx <jonas.snellinckx@gmail.com>
2022-10-03 09:55:19 +0200

    improvement: whitelist methods (#146)

--- src/refinable/contract/Erc1155WhitelistContract.ts
+++ src/refinable/contract/Erc1155WhitelistContract.ts
@@ -1,9 +1,10 @@
 import { ContractTypes } from "../../@types/graphql";
 import { RefinableEvmOptions } from "../../types/RefinableOptions";
-import { Contract, IContract } from "./Contract";
+import { WhitelistContract } from "./AbstractWhitelistContract";
+import { IContract } from "./Contract";
 import { Erc721WhitelistContract } from "./Erc721WhitelistContract";
 
-export class Erc1155WhitelistContract extends Contract {
+export class Erc1155WhitelistContract extends WhitelistContract {
   static type = ContractTypes.Erc1155WhitelistedToken as const;
   static deployArgsSchema = Erc721WhitelistContract.deployArgsSchema;
   constructor(contract: IContract, evmOptions: RefinableEvmOptions) {
